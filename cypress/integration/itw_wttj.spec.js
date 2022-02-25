33de05b3ce6eb363291fe7244c02edb06aa86791
Hadrien <hadrien.poquet@papernest.com>
2022-02-25 14:25:36 +0100

    login

--- cypress/integration/itw_wttj.spec.js
+++ cypress/integration/itw_wttj.spec.js
@@ -1,10 +1,39 @@
+//import email from '../fixtures/data.json'
+
+
 describe(`after visiting ["www.welcometothejungle.com/fr/me/settings/account"] webpage, clicking on ["Se connecter"] button, filling ["Email", "Mot de passe"] inputs, clicking ["Se connecter"] button, filling ["Photo de profil"] input and clicking on ["OK"] button`, () => {
+
   before(() => {
     cy.visit('fr/me/settings/account')
     // ...
   })
+
+  after(() => {
+    // ...
+  })
+
   it("_profile-avatar data are updated", () => {
     // ...
-    cy.wait(5500)
+    cy.url().should('contains', 'www.welcometothejungle.com/fr/signin')
+    
+    cy.get('.sc-cVAmsi > [data-testid="login-field-email"]')
+    .should('be.visible')
+    .type('inqom.qaautomationapplicant@gmail.com')
+
+    cy.get('[data-testid="login-field-password"]')
+    .should('be.visible')
+    .type('o5N,d5ZR@R7^')
+    
+    cy.get('[data-testid="login-button-submit"]')
+    .should('be.visible')
+    .click()
+    
+    cy.url().should('contains', 'www.welcometothejungle.com/fr/me/settings/account')
+
+
   })
+
+
+
+
 })
