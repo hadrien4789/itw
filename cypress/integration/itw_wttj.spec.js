bf53d104f4f145aa664039f3588ca9c7ec49ccaf
Hadrien <hadrien.poquet@papernest.com>
2022-02-25 14:50:08 +0100

    use data in data.json

--- cypress/integration/itw_wttj.spec.js
+++ cypress/integration/itw_wttj.spec.js
@@ -1,5 +1,4 @@
-//import email from '../fixtures/data.json'
-
+import data from "../fixtures/ids/data.json" 
 
 describe(`after visiting ["www.welcometothejungle.com/fr/me/settings/account"] webpage, clicking on ["Se connecter"] button, filling ["Email", "Mot de passe"] inputs, clicking ["Se connecter"] button, filling ["Photo de profil"] input and clicking on ["OK"] button`, () => {
 
@@ -18,18 +17,27 @@ describe(`after visiting ["www.welcometothejungle.com/fr/me/settings/account"] w
     
     cy.get('.sc-cVAmsi > [data-testid="login-field-email"]')
     .should('be.visible')
-    .type('inqom.qaautomationapplicant@gmail.com')
-
+    .type(data.email)
+ 
     cy.get('[data-testid="login-field-password"]')
     .should('be.visible')
-    .type('o5N,d5ZR@R7^')
+    .type(data.password)
+    
     
     cy.get('[data-testid="login-button-submit"]')
     .should('be.visible')
     .click()
     
     cy.url().should('contains', 'www.welcometothejungle.com/fr/me/settings/account')
+    
+    cy.wait(4000)
+    
+    //cy.get('.sc-fGoOlv > .sc-djWRfJ').attachFiles('integration/inqom.png')
 
+   // cy.get('.sc-fGoOlv > .sc-djWRfJ').attachFiles('cypress/fixtures/images/inqom.png')
+
+    //cy.wait(4000)
+    //cy.get('#file-upload').selectFile('cypress/fixtures/images/evening.png')
 
   })
 
